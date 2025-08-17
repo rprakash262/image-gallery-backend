2b452b51c6d952ee35ba34e65a9f6d534f037993
rprakash262 <rprakash262@gmail.com>
2025-08-17 10:40:06 +0530

    add new alloweed origin

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const app = express();
 
 app.use(express.json());
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://image-gallery-frontend-8a73.onrender.com"
+];
+
 //cors
 app.use(
   cors({
-    origin: "http://localhost:3000,https://image-gallery-frontend-8a73.onrender.com",
+    origin: allowedOrigins,
     methods: "GET,POST,PUT,PATCH,DELETE",
     credentials: true,
     allowedHeaders: "Content-Type,Authorization"
